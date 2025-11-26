bca15a6a695c6774dcb2bd1d43f79bdad8a282ca
Mohitkr09 <mkt27858@gmail.com>
2025-11-26 22:49:52 +0530

    ok

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL && import.meta.env.VITE_API_URL !== "undefined"
-    ? import.meta.env.VITE_API_URL
+  import.meta.env.VITE_API_BASE_URL &&
+  import.meta.env.VITE_API_BASE_URL !== "undefined"
+    ? import.meta.env.VITE_API_BASE_URL
     : "http://localhost:5000/api";
 
 const api = axios.create({
